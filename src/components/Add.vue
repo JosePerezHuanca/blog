8ed15c9a5a7a7f3a5d854678cb55a735e85bc3a8
Jose Perez Huanca <perezhuancajose@gmail.com>
2023-10-04 16:54:19 -0300

    Componente para crear un post

---
+++ src/components/Add.vue
@@ -0,0 +1,38 @@
+<template>
+    <h2>Crear post</h2>
+    <form v-on:submit.prevent="createPostMethod">
+        <input type="text" id="title" required v-model="title">
+        <label for="title">Título</label>
+        <input type="text" id="content" required v-model="content">
+        <label for="content">Contenido</label>
+        <button type="submit">Publicar</button>
+    </form>
+</template>
+<script>
+export default {
+    computed:{
+        token(){
+            return this.$store.getters.getToken;
+        }
+    },
+    data(){
+        return{
+            title: '',
+            content: ''
+        }
+    },
+    methods:{
+        createPostMethod(){
+            const payload={
+                token: this.token,
+                postObj:{
+                    title: this.title,
+                    content: this.content
+                }
+            }
+            this.$store.dispatch('createPost', payload);
+            
+        }
+    }
+}
+</script>
